d2147ef12b7b142d7299e2a066124a6ef5fc6f7f
Thomas Jollans <thomas@jollans.com>
2010-04-20 20:44:28 +0200

    Don't include ANY boiler-plate files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@ echo +++ Running aclocal ... &&
 aclocal &&
 echo +++ Running autoconf ... &&
 autoconf && 
+echo +++ Running automake --add-missing ... &&
+automake --add-missing
 echo +++ Running automake ... &&
 automake --foreign Makefile source/Makefile &&
 echo You may now run ./configure ||
