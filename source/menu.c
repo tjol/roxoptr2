ee9f3cde7b6f056aa75dffa9993e0a05f3f77ebd
Thomas Jollans <thomas@jollans.com>
2010-05-11 12:39:26 +0200

    menu: fix ordering v-a-v "Credits"

--- source/menu.c
+++ source/menu.c
@@ -99,7 +99,7 @@ void init_menu()
     mcredits->exec = &credits_page;
     
     mquit->text = "Quit";
-    mquit->up   = mlevels;
+    mquit->up   = mcredits;
     mquit->down = mquit;
     mquit->exec = &quitgame;
     
